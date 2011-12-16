1ddfc54cf8f9a39b970f4b5ec96407720c2a5be3
Tony Issakov <tony@thefrontiergroup.com.au>
2011-12-16 20:28:51 +0800

    attempt to get past a freeze

--- packages.rb
+++ packages.rb
@@ -1,5 +1,5 @@
 dep 'jenkins.managed' do
-  requires 'jenkins apt key', 'jenkins apt source'
+  requires 'jenkins apt key', 'jenkins apt source', 'java.managed'
   installs { via :apt, 'jenkins'}
   provides []
 end
