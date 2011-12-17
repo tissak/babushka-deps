d707d2106282d5841dee0c6e4ef982bae9baf7f1
Tony Issakov <tony@thefrontiergroup.com.au>
2011-12-17 08:25:59 +0800

    minor fixes

--- jenkins.rb
+++ jenkins.rb
@@ -1,9 +1,12 @@
 dep 'jenkins apt key' do
-  met? { !(sudo("apt-key list").grep(//).nil?) }
+  met? { !(sudo("apt-key list").grep(/Kawaguchi/).nil?) }
   meet { sudo("wget -q -O - http://pkg.jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -")}
 end
 
 dep 'jenkins apt source' do
   met? { File.exist?("/etc/apt/sources.list.d/jenkins.list") }
-  meet { sudo("echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list && aptitude update")}
+  meet {
+    sudo("echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list")
+    sudo("aptitude update")
+  }
 end
