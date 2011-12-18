62adcbbbf533c5f4e0df80421043768a989a450e
Tony Issakov <tony@thefrontiergroup.com.au>
2011-12-18 08:53:41 +0800

    reqwrite to use wget

--- rvm.rb
+++ rvm.rb
@@ -1,12 +1,11 @@
 dep 'rvm user' do
   met? { which 'rvm' }
-  meet { shell('bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )')}
+  meet { shell('bash -c "`wget -O - https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer`"')}
   after { shell("echo '[[ -s \"$HOME/.rvm/scripts/rvm\" ]] && . \"$HOME/.rvm/scripts/rvm\" # Load RVM function' >> ~/.bash_profile") }
 end
 
 dep 'rvm system' do
-  requires 'curl.managed'
   met? { which 'rvm' }
-  meet { sudo('bash < <(curl -s https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer )')}
+  meet { sudo('bash -c "`wget -O - https://raw.github.com/wayneeseguin/rvm/master/binscripts/rvm-installer`"')}
   after { shell("echo '[[ -s \"/usr/local/rvm/scripts/rvm\" ]] && . \"/usr/local/rvm/scripts/rvm\" # Load RVM function' >> ~/.bash_profile") }
 end
