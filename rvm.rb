a28dfa0289f8d9176a0c766aed4107d2b48a6191
Tony Issakov <tony@thefrontiergroup.com.au>
2011-12-19 13:57:01 +0800

    grab curl for later use

--- rvm.rb
+++ rvm.rb
@@ -1,5 +1,6 @@
 #system install of rvm using a gist script
 dep 'rvm system' do
+  requires 'tissak:curl.managed'
   met? { File.exist?("/usr/local/rvm") }
   meet { sudo('bash -c "`wget -O - https://gist.github.com/raw/2d253db7c2564f3d6388/ebb283b618f569172488cd760e5c2da5b4b918ba/rvm-installer.sh`"')}
 end
